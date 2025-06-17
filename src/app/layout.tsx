59a97d9a6294d6d55baf23e4c526097b6d7d4bdb
Kaialan <satyajit1501@gmail.com>
2025-06-17 12:22:10 +0530

    fix: changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import LayoutContext from "@/components/layout-context";
-import { LoadingProvider } from "@/components/loadingContext";
+// import LayoutContext from "@/components/layout-context";
+// import { LoadingProvider } from "@/components/loadingContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,11 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <LoadingProvider>
-          <LayoutContext>
+        {/* <LoadingProvider> */}
+          {/* <LayoutContext> */}
           {children}
-          </LayoutContext>
-        </LoadingProvider>
+          {/* </LayoutContext> */}
+        {/* </LoadingProvider> */}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,41 +3,41 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import React, { useEffect } from "react";
-import { useLoading } from "@/components/loadingContext";
+// import React, { useEffect } from "react";
+// import { useLoading } from "@/components/loadingContext";
 
 export default function LandingPage() {
-  const { setPageLoaded } = useLoading();
-  const MIN_LOAD_TIME = 3000;
-  const backgroundImage = "/assets/images/cover-picture.jpg";
+  // const { setPageLoaded } = useLoading();
+  // const MIN_LOAD_TIME = 3000;
+  const backgroundImage = "/assets/images/cover-picture-2.jpg";
 
-  useEffect(() => {
-    const imageLoadPromise = new Promise<void>((resolve, reject) => {
-      const img = new window.Image();
-      img.src = backgroundImage;
-      img.onload = () => {
-        resolve();
-      };
-      img.onerror = () => {
-        reject(new Error("Image load failed"));
-      };
-    });
+  // useEffect(() => {
+  //   const imageLoadPromise = new Promise<void>((resolve, reject) => {
+  //     const img = new window.Image();
+  //     img.src = backgroundImage;
+  //     img.onload = () => {
+  //       resolve();
+  //     };
+  //     img.onerror = () => {
+  //       reject(new Error("Image load failed"));
+  //     };
+  //   });
 
-    const minTimePromise = new Promise<void>((resolve) => {
-      setTimeout(() => {
-        resolve();
-      }, MIN_LOAD_TIME);
-    });
+  //   const minTimePromise = new Promise<void>((resolve) => {
+  //     setTimeout(() => {
+  //       resolve();
+  //     }, MIN_LOAD_TIME);
+  //   });
 
-    Promise.all([imageLoadPromise, minTimePromise])
-      .then(() => {
-        setPageLoaded(true);
-      })
-      .catch((error) => {
-        console.error("Loading sequence failed:", error);
-        setPageLoaded(true);
-      });
-  }, [backgroundImage, setPageLoaded]);
+  //   Promise.all([imageLoadPromise, minTimePromise])
+  //     .then(() => {
+  //       setPageLoaded(true);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Loading sequence failed:", error);
+  //       setPageLoaded(true);
+  //     });
+  // }, [backgroundImage, setPageLoaded]);
 
   return (
     <div
@@ -50,19 +50,19 @@ export default function LandingPage() {
     >
       {/* Logo */}
       <div className="absolute top-8 left-8 z-10">
-        <img src="/logo/logo-light-1.png" alt="Logo" className="h-16 w-auto" />
+        <img src="/logo/logo-light-1.png" alt="Logo" className="h-10 md:h-16 w-auto" />
       </div>
       
       {/* Headline */}
-      <div className="flex items-center justify-center min-h-screen">
-        <h1 className="text-6xl md:text-8xl font-black text-white text-center tracking-tight">
+      <div className="flex items-center justify-center min-h-screen px-8">
+        <h1 className="w-full text-4xl md:text-7xl font-bold text-white text-left text-wrap tracking-tight">
           Grind. Growth. Gratitude.
         </h1>
         <Link
           href="http://linkedin.com/in/whynikhil"
           className="cursor-pointer"
         >
-          <Button className="absolute bottom-10 right-10 px-8 py-8 bg-white text-black text-lg rounded-full hover:bg-gray-100">
+          <Button className="absolute bottom-10 right-10 py-6 px-6 bg-white text-black text-lg rounded-md hover:bg-gray-100 backdrop-blur-md">
             <span>
               <Image
                 src="/assets/svg/linkedin.svg"
@@ -72,7 +72,7 @@ export default function LandingPage() {
                 className="w-8 h-8 rounded-full"
               />
             </span>
-            Nikhil Kakkar
+            Nikhil
           </Button>
         </Link>
       </div>
