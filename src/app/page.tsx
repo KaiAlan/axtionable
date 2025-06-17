d949f2af74bef4106806c8ef8824788478e07766
Kaialan <satyajit1501@gmail.com>
2025-06-17 19:36:20 +0530

    fix: changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-'use client'
+"use client";
 
 import { Button } from "@/components/ui/button";
+// import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 // import React, { useEffect } from "react";
+// import React, { useState } from "react";
 // import { useLoading } from "@/components/loadingContext";
 
 export default function LandingPage() {
   // const { setPageLoaded } = useLoading();
   // const MIN_LOAD_TIME = 3000;
+  // const [isHovered, setIsHovered] = useState(false);
   const backgroundImage = "/assets/images/cover-picture-2.jpg";
 
   // useEffect(() => {
@@ -50,29 +53,40 @@ export default function LandingPage() {
     >
       {/* Logo */}
       <div className="absolute top-8 left-8 z-10">
-        <img src="/logo/logo-light-1.png" alt="Logo" className="h-10 md:h-16 w-auto" />
+        <img
+          src="/logo/logo-light-1.png"
+          alt="Logo"
+          className="h-10 md:h-12 w-auto"
+        />
       </div>
-      
+
       {/* Headline */}
       <div className="flex items-center justify-center min-h-screen px-8">
-        <h1 className="w-full text-4xl md:text-7xl font-bold text-white text-left text-wrap tracking-tight">
+        <h1 className="absolute left-8 bottom-72 w-full text-4xl md:text-7xl font-bold text-white text-left text-wrap tracking-tight">
           Grind. Growth. Gratitude.
         </h1>
         <Link
           href="http://linkedin.com/in/whynikhil"
-          className="cursor-pointer"
+          className="cursor-pointer absolute bottom-10 right-10"
         >
-          <Button className="absolute bottom-10 right-10 py-6 px-6 bg-white text-black text-lg rounded-md hover:bg-gray-100 backdrop-blur-md">
-            <span>
+          <Button
+            className={`
+        relative overflow-hidden py-6 px-4 bg-black hover:bg-black hover:shadow-[4px_4px_0px_#4A4A4A] text-white text-lg rounded-full before:absolute before:top-0 before:left-[-100%] before:w-full before:h-full
+        before:bg-white before:transition-all before:duration-500 before:ease-in-out
+        before:[clip-path:polygon(10%_0,70%_0,90%_100%,30%_100%)]
+        hover:before:left-full cursor-pointer
+      `}
+          >
+            <span className="w-8 h-8 rounded-full overflow-clip">
               <Image
                 src="/assets/svg/linkedin.svg"
                 alt="linkedin-logo"
-                width={1}
-                height={1}
-                className="w-8 h-8 rounded-full"
+                width={0}
+                height={0}
+                className="w-8 h-8 rounded-full object-cover"
               />
             </span>
-            Nikhil
+            <span className="relative z-10">Nikhil</span>
           </Button>
         </Link>
       </div>
